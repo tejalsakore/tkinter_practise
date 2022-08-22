59a0b2de530a27c47f0ed1b04c74a945173db148
tejalsakore <sakoretejal1511@gmail.com>
2022-08-22 17:08:41 +0530

    updated button

--- 5.button_widget.py
+++ 5.button_widget.py
@@ -4,8 +4,9 @@ window = Tk()
 window.title("GUI")
 l1 = Label(window,text="Press Enter",font=("Arial",20))
 window.geometry('500x500')
-l1.grid(column=0,row=5)
+l1.grid(column=1,row=5)
 
-bt = Button(window,text="Enter")
-bt.grid(column=5,row=5)
+bt = Button(window,text="Enter",bg="yellow",fg="red")
+bt.grid(column=1,row=6)
+
 mainloop()
