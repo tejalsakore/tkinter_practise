f72a59fda98e1602a5d8948ac5ff9159e6ab9f36
tejalsakore <sakoretejal1511@gmail.com>
2022-08-22 17:00:48 +0530

    updated label

--- 3.label_widget.py
+++ 3.label_widget.py
@@ -3,5 +3,5 @@ from tkinter import Tk,Label,mainloop
 window = Tk()
 window.title("GUI") 
 i1 =Label(window,text="Heyyyaa!",font=("Arial Bold",50))
-i1.grid(column=0,row=0)  #grid()->set thr position of Label
+i1.grid(column=0,row=0)  #grid()->set the position of Label
 mainloop()
