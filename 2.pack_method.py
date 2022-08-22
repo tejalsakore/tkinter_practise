708fe725979b58413910e76cafe97206862fdfd5
tejalsakore <sakoretejal1511@gmail.com>
2022-08-22 12:40:20 +0530

    added lable widget

--- 2.pack_method.py
+++ 2.pack_method.py
@@ -1,4 +1,5 @@
 import tkinter
 window = tkinter.Tk()
+window.title("GUI") 
 label = tkinter.Label(window, text="Hey Tkinter!").pack() #pack is used to show object in the window
 window.mainloop()
---
+++ 3.label_widget.py
@@ -0,0 +1,7 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("GUI") 
+i1 =tkinter.Label(window,text="Heyyyaa!",font=("Arial Bold",50))
+i1.grid(column=0,row=0)
+window.mainloop()
