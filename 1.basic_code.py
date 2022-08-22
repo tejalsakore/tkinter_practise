2269ac11181fc9888fe111410864adf23d81efc5
tejalsakore <sakoretejal1511@gmail.com>
2022-08-22 12:27:36 +0530

    added basic code

---
+++ 1.basic_code.py
@@ -0,0 +1,4 @@
+import tkinter
+window = tkinter.Tk()       #To create a main window, tkinter offers a method & window is the name of the main window object
+window.title("GUI")         #to rename the title of the window
+window.mainloop()           # mainloop() is used when your application is ready to run. mainloop() is an infinite loop used to run the application, wait for an event to occur and process the event as long as the window is not closed.
---
+++ 2.pack_method.py
@@ -0,0 +1,4 @@
+import tkinter
+window = tkinter.Tk()
+label = tkinter.Label(window, text="Hey Tkinter!").pack() #pack is used to show object in the window
+window.mainloop()
