eef9d62e02270d5d9e8f50198f6adc1b2054e49b
tejalsakore <sakoretejal1511@gmail.com>
2022-08-25 00:52:38 +0530

    updated scrolledtext

--- 11.scrolledtext_widget.py
+++ 11.scrolledtext_widget.py
@@ -1,4 +1,4 @@
-from tkinter import Tk,mainloop
+from tkinter import Tk,mainloop,INSERT
 from tkinter.scrolledtext import ScrolledText
 
 window = Tk()
@@ -6,6 +6,7 @@ window.title("ScrolledText Widget")
 window.geometry('500x500')
 
 st =ScrolledText(window,width=50,height=10)
+st.insert(INSERT,"this is srcollbar")
 st.grid(column=0,row=0)
 
 mainloop()
